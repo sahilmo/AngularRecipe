00a9fca41cc5da1fffe0a1504ca6bbe268acf081
sahil <sahil_mohammad@outlook.com>
2019-04-16 19:09:43 +0400

    Initial commit

---
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
+
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
+})
+export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+  // @Output() recipeSelected = new EventEmitter<void>();
+
+  constructor(private recipeService: RecipeService) { }
+
+  ngOnInit() {
+  }
+
+  onSelected() {
+    // this.recipeSelected.emit();
+    this.recipeService.recpieSelected.emit(this.recipe);
+  }
+
+}
