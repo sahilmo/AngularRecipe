5f46865f534dc69f766161b842f4565fb5fffdc8
sahil <sahil_mohammad@outlook.com>
2019-04-17 16:25:56 +0400

    shopping list service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,26 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 
 export class RecipeService {
     recpieSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe('A Test Recipe',
+         'This is simply a test',
+          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+          , [
+              new Ingredient('Meat', 1),
+              new Ingredient('Ginger', 20 ),
+          ]),
+        new Recipe('Another Test Recipe',
+         'This is simply a test',
+          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+          , [
+            new Ingredient('Chicken', 1),
+            new Ingredient('Onion', 20 ),
+        ])
     ];
 
     getRecipes() {
